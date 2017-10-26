103d8cb66075e72459e2effabea28d777f183e94
Sam Whang <sangwoowhang@gmail.com>
2017-10-26 09:26:24 -0500

    moving stuff around and adding headers

--- devicematcher.py
+++ devicematcher.py
@@ -1,31 +1,47 @@
+'''
+Author  : Sam Whang
+Filename: DeviceMatcher.py
+License : MIT
+Usage   : compares interface signatures and serializes them to
+          an abbreviated signature
+'''
 import re
+
+# any devices matching the first indices in each list
+# will conform to the second index after pattern match
 interfaces = [
-    [["TenGigabitEthernet","Te"],"Te"],
-    [["GigabitEthernet","Gi"],"Gi"],
-    [["FastEthernet","Fa"],"Fa"],
-    [["Ethernet","Eth"],"Eth"],
-    [["Port-channel","Po"],"Po"],
-    [["Serial"],"Ser"],
+    [["TenGigabitEthernet", "Te"], "Te"],
+    [["GigabitEthernet", "Gi"], "Gi"],
+    [["FastEthernet", "Fa"], "Fa"],
+    [["Ethernet", "Eth"], "Eth"],
+    [["Port-channel", "Po"], "Po"],
+    [["Serial"], "Ser"],
 ]
+
 def deviceRegex(string):
     pattern = r'([a-zA-Z\-]*)(\d+[\/\d\:]*)'
     result = re.match(pattern, string)
-    try:
-        groups = result.groups()
-        print(list(groups)[1])
-    except:
-        raise
-
-def convertName(string):
-    pattern = r'[a-zA-Z\-]*(\d+[\/\d\:]*)'
-    result = re.match(pattern, string)
     if result:
-        print(result.groups())
-        name, number = list(result.groups())
-        print(name, number)
+        device, number = list(result.groups())
+        stop = False
+        # iterate through interfaces to find matches -- stops when found
+        for interface, conversion in interfaces:
+            for i in interface:
+                
+                # sanitize the strings before comparison
+                if device.lower() == i.lower():
+                    print('Converting: {:<30} -> {}{}'.format(
+                        string,
+                        conversion,
+                        number))
+                    stop = True
+                    found = True
+            if stop:
+                break
     else:
-       exit('No matches')
+        print('No known conversion names for this device: {}'.format(string))
+
 deviceRegex('GigabitEthernet1/3')
 deviceRegex('tengigabitethernet7/1/25')
 deviceRegex('Serial1/0/0/15:1')
-
+deviceRegex('asdfaf')
--- pytree.py
+++ pytree.py
@@ -2,29 +2,51 @@ __author__  = "Sam WHang | WGB"
 __email__   = "sangwoowhang@gmail.com"
 __license__ = "MIT"
 
-
 import os
 
-#blue for directories
-folder='\x1b[0;34;40m'
-yellow='\x1b[0;33;40m'
-red='\x1b[1;31;40m'
-end ='\x1b[0m'
-
-skip = ["__pycache__","migrations"]
-exts = ['html','css','py']
 def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
+    '''
+    walks through directory with os.walk and lists directories 
+    and files with given extensions
+    '''
+    # color code directories and specific extensions
+    # blue for directories  
+    folder='\x1b[0;34;40m'
+    yellow='\x1b[0;33;40m'
+    lightyellow = '\x1b[0;93;40m'
+    magenta='\x1b[1;31;40m'
+    cyan='\x1b[1;36;40m'
+    blue = '\x1b[1;34;40m'
+    # green
+    lightcyan = '\x1b[1;96;40m'
+    end ='\x1b[0m'
+
+    skip = ("__pycache__", "migrations", '.git')
+    exts = {
+        'html': blue,
+        'css': cyan, 
+        'py': lightyellow, 
+        'md': lightcyan,
+    }
+    for root, dirs, files in os.walk(startpath, topdown=True):
+        dirs[:] = [d for d in dirs if d not in skip]
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
+
         if os.path.basename(root) not in skip:
+            # prints folder
             print('{}{}{}/{}'.format(folder,indent, os.path.basename(root),end))
             subindent = ' ' * 4 * (level + 1)
+
+            # color codes file before print
             for f in files:
-                color=''
-                if f.endswith(exts[0]): color = red
-                if f.endswith('html'): color = yellow
-                print('{}{}{}{}'.format(color,subindent, f,end))
+                for ext in exts:
+                    if f.endswith(ext):
+                        print('{}{}{}{}'.format(exts[ext], subindent, f, end))
+                        break
+                else:
+                    print('{}{}'.format(subindent, f))
+
 
 if __name__ == "__main__":
     list_files(os.getcwd())
