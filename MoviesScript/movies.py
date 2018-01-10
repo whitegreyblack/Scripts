3c5e61087b133e66735d5bfc1f353adfb96f9991
Sam Whang <sangwoowhang@gmail.com>
2018-01-10 13:43:13 -0600

    Added documentation strings to movies.py

--- MoviesScript/movies.py
+++ MoviesScript/movies.py
@@ -10,14 +10,18 @@ import sys
 from bs4 import BeautifulSoup
 
 def vc_listing():
+    '''Retrieves movie list from valley-city section'''
     link = http://bison6cinema.com/valley-city/
 
 def jt_listing():
+    '''Retrieves movie list from jamestown section'''
     link = http://bison6cinema.com
 
 def upcoming():
+    '''Retrieves upcoming movie lsit from jamestown section'''
     link = http://bison6cinema.com
     
+# dictionary used during args parsing to specify individual functions
 listings = {
     'vc': vc_listing,
     'jt': jt_listing,
@@ -25,6 +29,9 @@ listings = {
 }
 
 def movies():
+    '''Main driver that calls vc, jt, and upcoming functions
+    Input arguments specified are parsed here for running commands
+    '''
     if len(sys.argv) == 1:
         vc_listing()
         jt_listing()
