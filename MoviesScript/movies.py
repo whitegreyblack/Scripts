8b54b30fd4f9d2dfbbbf2aedda9d125e9522dbda
Sam Whang <sangwoowhang@gmail.com>
2018-02-15 09:47:51 -0600

    Finished converting bash movie script into python

--- MoviesScript/movies.py
+++ MoviesScript/movies.py
@@ -5,22 +5,48 @@
 # Author  : Sam Whang | WGB
 # License : MIT
 
-import os
-import sys
+from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
+import requests
+import sys
+import os
+
+def parser(soup, tag, cls):
+    for title in soup.find_all(tag, {'class': cls} if cls else {}):
+        try:
+            text.append(title.get_text())
+        except KeyError:
+            raise
 
 def vc_listing():
     '''Retrieves movie list from valley-city section'''
-    link = http://bison6cinema.com/valley-city/
+    text.append('--- Valley City Listings ---')
+    link = "http://bison6cinema.com/valley-city/"
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    parser(soup, 'h4', 'heading14')
 
 def jt_listing():
     '''Retrieves movie list from jamestown section'''
-    link = http://bison6cinema.com
+    text.append('--- Jamestown Listings  ---')
+    link = "http://bison6cinema.com"
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    parser(soup, 'h4', 'heading18')
 
 def upcoming():
     '''Retrieves upcoming movie lsit from jamestown section'''
-    link = http://bison6cinema.com
-    
+    text.append('---   Upcoming Movies   ---')
+    link = "http://bison6cinema.com"
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    for title in soup.find_all('span'):
+        if not title.attrs:
+            for string in title.stripped_strings:
+                text.append(string)
+
+text = []
+
 # dictionary used during args parsing to specify individual functions
 listings = {
     'vc': vc_listing,
@@ -33,16 +59,18 @@ def movies():
     Input arguments specified are parsed here for running commands
     '''
     if len(sys.argv) == 1:
-        vc_listing()
-        jt_listing()
-        upcoming()
+        for link in listings.values():
+            link()
 
     else:
         try:
-            listings[sys.argv[1]]()
+            listings[sys.argv[1].replace('-', '')]()
         except KeyError:
             print('Parameter input not recognized', sys.argv[1])
 
+    if text:
+        print("\n".join(text))
+
 if __name__ == "__main__":
-    print(len(sys.argv), sys.argv)
+    # print(len(sys.argv), sys.argv)
     movies()
