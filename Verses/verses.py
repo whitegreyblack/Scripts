1aa76105dcd2770e1c66150bdf82dde592728110
Sam Whang <sangwoowhang@gmail.com>
2018-02-15 08:56:36 -0600

    Daily Verse Thu_Feb_15

--- Verses/verses.py
+++ Verses/verses.py
@@ -28,7 +28,7 @@ def verse():
         except KeyError:
             pass
     
-    print('Image Links Founds: {}'.format(len(img_urls)))
+    print(f'\033[1AImage Links Founds: {len(img_urls)}')
     
     # retrieve the image using given url link
     for url in img_urls:
@@ -37,11 +37,11 @@ def verse():
         file = [url.split('/')[-1]]
         path = os.path.realpath(__file__).split('\\')[:-1]
         final_path = "/".join(path + file)
-
-        print('Retrieving: {} @ {}'.format(url, final_path))
-        urlretrieve(url, final_path)
+        if not os.path.exists(final_path) or (os.path.exists(final_path) and not os.path.isfile(final_path)):
+            print(f'\033[1ARetrieving: {url} @ {final_path}')
+            urlretrieve(url, final_path)
     
-    exit('Finished')
+    exit('\033[1AFinished')
 
 if __name__ == "__main__":
     print(os.path.realpath(__file__))
