eeb262c8d4dfae5bfeeeee8cee4f716f13363ef8
Sam Whang <sangwoowhang@gmail.com>
2018-05-21 14:36:31 -0500

    Modified parse date to wrap datetime string parser

--- Accounts/funcutils.py
+++ Accounts/funcutils.py
@@ -12,38 +12,20 @@ Date contains invalid number of variables (expected: 3, got {})
 # -- START FUNCTION LIST --
 def parse_date(date: str) -> datetime.date:
     """Error checking for user input dates"""
-    # checks seperator - only want slashes or dashes
-    if "/" in date:
-        date = date.split("/")
-    elif "-" in date:
-        date = date.split("-")
-    else:
-        raise ValueError("Date contains invalid seperator")
+    date_model = datetime.datetime.strptime(date, date_format)
+    if date_model.year > datetime.datetime.now().year % 2000:
+        date_model.replace(year=date_model.year - 100)
+    return date_model.date()
 
-    # checks date variables between seperators
-    try:
-        date = tuple(map(lambda x: int(x), date))
-    except ValueError as exception:
-        exit("Date contains invalid variables types")
-
-    try:
-        month, day, year = date
-    except ValueError:
-        exit(INVALID_VARIABLE_NUMBER.format(len(date)))
-
-    # All errors have been safely checked -- now create a valid date object
-    return datetime.date(year=date[2], month=date[0], day=date[1])
-
-def valid_date(date, seperator="/") -> bool:
+def valid_date(date: str, start: str, end: str=None, seperator="/") -> bool:
     """Date parsing used in csv date inputs"""
-    date = tuple(map(lambda x: int(x), date.split(seperator)))
-    date = datetime.date(year=2000 + date[2],
-                         month=date[0],
-                         day=date[1])
+    date = parse_date(date)
+    if not isinstance(start, datetime.date):
+        start = parse_date(start)
+    if not end:
+        end = datetime.datetime.now().date()
     return start <= date <= end
 # -- END OF FUNCTIONS --
 
 if __name__ == "__main__":
     print("Utils for date time manipulation in accounts.py and creditcard.py")
-    # print(parse_date("10/a/6/6"))
-    # print(parse_date("10/5/6/6"))
