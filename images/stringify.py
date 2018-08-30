5dcdec965d49fcdd429a4d3705cde098d780fc97
Sam Whang <sangwoowhang@gmail.com>
2018-08-29 22:34:13 -0700

    Added image to images folder for usage in stringify

--- images/stringify.py
+++ images/stringify.py
@@ -1,29 +1,31 @@
-# using pil - transforms an image from png to a python string to be used as a map
+# name: stringify.py
+# desc: transforms an image from png to a list of strings for terminal printing
 from PIL import Image
 
-def stringify(string, debug=False):
-    with Image.open(string) as img:
-        pixels = img.load()
-        w, h = img.size
+def stringify(image_path, debug=False):
+    with Image.open(image_path) as image:
+        pixels = image.load()
+        w, h = image.size
         lines = []
+        
+        # row major, add line by line
         for j in range(h):
             line = ""
             for i in range(w):
+                # check for alpha channel
                 try:
                     r, g, b, _ = pixels[i,j]
                 except:
                     r, g, b = pixels[i, j]
-                if (r, g, b)== (0,0,0):
-                    line += "#"
-                # add some trees
-                # buildings
-                # some other stuff
-                else:
-                    line += "."
+            
+                # separate by colorless pixels
+                line += "#" if (r, g, b) == (0, 0, 0) else "."
             lines.append(line)
+
         if debug:
             print("\n".join(lines))
+
         return lines
 
 if __name__ == "__main__":
-    stringify("./assets/testmap.png", debug=True)
+    stringify("testmap.png", debug=True)
